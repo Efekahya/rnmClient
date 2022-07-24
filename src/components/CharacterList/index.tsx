ecd99660df2001550ec27e3b60772105708603d6
Efe Kahyaoğlu <75262924+Efekahya@users.noreply.github.com>
2022-07-24 18:06:29 +0300

    CharacterList component created

--- src/components/CharacterList/index.tsx
+++ src/components/CharacterList/index.tsx
@@ -1,6 +1,43 @@
 import React from "react"
-import "./styles.scss"
 
-export default function CharacterList() {
-  return <div>CharacterList</div>
+interface ICharacter {
+  name: string
+  id: number
+  gender: string
+  spacies: string
+  origin: string
+  type: string
+  image: string
+  location: string
+  episodes: object[]
+}
+interface CharacterListProps {
+  characters: ICharacter[] //TODO Characterler objesi gelince character tipi ile değiştirilecek
+  count: number
+}
+export default function CharacterList({
+  characters,
+  count,
+}: CharacterListProps) {
+  if (count !== -1) {
+    characters = characters.slice(0, count)
+  }
+  return (
+    <>
+      <div className="characterList">
+        {characters.map((character) => (
+          <div className="character" key={character.id}>
+            <span>
+              <img
+                className="image"
+                src={character.image}
+                alt={character.name}
+              />
+            </span>
+            <span>{character.name}</span>
+          </div>
+        ))}
+      </div>
+    </>
+  )
 }
