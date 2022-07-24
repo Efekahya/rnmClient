13462f92faf041a847ca8a282fef09be5b6c79ab
Efe Kahyaoğlu <75262924+Efekahya@users.noreply.github.com>
2022-07-24 18:00:42 +0300

    Episode card component created and added breakpoint variables to variables.scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,44 @@
 import React from "react"
 
 import Navbar from "./components/Navbar/index"
-import { ReactComponent as Logo } from "./assets/profile.svg"
+import { ReactComponent as DigiEggs } from "./assets/digieggs.svg"
+import EpisodeCard from "./components/EpisodeCard"
+
+interface ICharacter {
+  name: string
+  id: number
+  gender: string
+  spacies: string
+  origin: string
+  type: string
+  image: string
+  location: string
+  episodes: object[]
+}
+
+const character: ICharacter = {
+  name: "Rick Sanchez",
+  id: 123,
+  gender: "male",
+  spacies: "Human",
+  origin: "Human",
+  type: "",
+  image: "./rick.png",
+  location: "Earth",
+  episodes: [{ name: "ep1" }],
+}
+
 function App() {
   return (
     <div className="App">
-      <Navbar Logo={<Logo />} />
+      <Navbar Logo={<DigiEggs />} />
+
+      <EpisodeCard
+        episode="S2.E10"
+        date="Fri, July 32, 2020"
+        title="The End of Something"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore incididunt ut laboreincididunt ut laboreet dolore..."
+      />
     </div>
   )
 }
