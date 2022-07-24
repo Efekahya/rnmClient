78ef45867077ad977a15f7e5e373b12d8d9f12d7
Efe Kahyaoğlu <75262924+Efekahya@users.noreply.github.com>
2022-07-24 18:01:00 +0300

    Revert "Episode card component created and added breakpoint variables to variables.scss"
    This reverts commit 13462f92faf041a847ca8a282fef09be5b6c79ab.

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,11 @@
 import React from "react"
 
 import Navbar from "./components/Navbar/index"
-import { ReactComponent as DigiEggs } from "./assets/digieggs.svg"
-import EpisodeCard from "./components/EpisodeCard"
-
-interface ICharacter {
-  name: string
-  id: number
-  gender: string
-  spacies: string
-  origin: string
-  type: string
-  image: string
-  location: string
-  episodes: object[]
-}
-
-const character: ICharacter = {
-  name: "Rick Sanchez",
-  id: 123,
-  gender: "male",
-  spacies: "Human",
-  origin: "Human",
-  type: "",
-  image: "./rick.png",
-  location: "Earth",
-  episodes: [{ name: "ep1" }],
-}
-
+import { ReactComponent as Logo } from "./assets/profile.svg"
 function App() {
   return (
     <div className="App">
-      <Navbar Logo={<DigiEggs />} />
-
-      <EpisodeCard
-        episode="S2.E10"
-        date="Fri, July 32, 2020"
-        title="The End of Something"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore incididunt ut laboreincididunt ut laboreet dolore..."
-      />
+      <Navbar Logo={<Logo />} />
     </div>
   )
 }
--- src/components/EpisodeCard/index.tsx
+++ src/components/EpisodeCard/index.tsx
@@ -1,48 +1,6 @@
 import React from "react"
-import { ReactComponent as StarIcon } from "../../assets/star.svg"
-import { ReactComponent as StarFilledIcon } from "../../assets/starFilled.svg"
-interface IEpisodeCardProps {
-  episode: string
-  date: string
-  title: string
-  description: string
-}
+import "./styles.scss"
 
-export default function EpisodeCard({
-  episode,
-  date,
-  title,
-  description,
-}: IEpisodeCardProps) {
-  const [isFavorited, setIsFavorited] = React.useState(false)
-  return (
-    <div className="episodeContainer">
-      <div className="psuedo">
-        <button
-          className="favoriteButton"
-          onClick={() => {
-            setIsFavorited(!isFavorited)
-          }}
-        >
-          {isFavorited ? (
-            <StarFilledIcon className="inline" />
-          ) : (
-            <StarIcon className="inline" />
-          )}
-        </button>
-      </div>
-      <button className="card">
-        <div className="header">
-          <div className="info">
-            <div className="episode title">{episode}</div>
-            <div className="date">{date}</div>
-          </div>
-        </div>
-        <div className="body">
-          <div className="title">{title}</div>
-          <div className="secondaryText">{description}</div>
-        </div>
-      </button>
-    </div>
-  )
+export default function EpisodeCard() {
+  return <div>EpisodeCard</div>
 }
