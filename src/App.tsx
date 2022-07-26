f493db25b134733d87e2969ca0df3106c1c5f951
Efe Kahyaoğlu <75262924+Efekahya@users.noreply.github.com>
2022-07-27 01:08:27 +0300

    made App.tsx ready to display the component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,16 @@ import React from "react"
 
 import Navbar from "./components/Navbar/index"
 import { ReactComponent as Logo } from "./assets/profile.svg"
+import EpisodeCard from "./components/EpisodeCard"
 function App() {
   return (
     <div className="App">
-      <Navbar Logo={<Logo />} />
+      <EpisodeCard
+        date="Fri, July 32, 2020"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore..."
+        episode="S2.E10"
+        title="The End of Something"
+      />
     </div>
   )
 }
