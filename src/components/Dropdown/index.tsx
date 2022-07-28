6f541828eac66ea090903dadbc3228e145ab716e
Efe Kahyaoğlu <75262924+Efekahya@users.noreply.github.com>
2022-07-28 13:32:08 +0300

    resolved pr reviews

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -1,33 +1,39 @@
 import React, { useState } from "react";
-import "./styles.scss";
 import { IDropdownProps } from "../../types/interfaces";
 import { ReactComponent as Arrow } from "../../assets/arrowDown.svg";
+
 export default function Dropdown({ items, selected }: IDropdownProps) {
   const [isOpen, setIsOpen] = useState(false);
+
   const handleDropdown = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
-    setIsOpen(!isOpen);
+    setIsOpen(isOpen => !isOpen);
   };
+  const click = (
+    event: React.MouseEvent<HTMLButtonElement>,
+    func: (event: React.MouseEvent<HTMLButtonElement>) => void
+  ) => {
+    func(event);
+    setIsOpen(false);
+  };
+
   return (
-    <div className={"container"}>
+    <div className="container">
       <button className="dropdown" onClick={handleDropdown}>
         {selected}
-        <Arrow className={"arrow " + (isOpen ? "down" : "")} />
+        <Arrow className={`arrow ${isOpen ? "down" : ""}`} />
       </button>
       <div className="psuedo">
-        <div className={"itemsContainer " + (isOpen ? "open" : "")}>
-          {items.map(item => (
+        <div className={`itemsContainer ${isOpen ? "open" : ""}`}>
+          {items.map(({ value, handleClick, label }) => (
             <div className="item">
               <button
-                key={item.value}
-                value={item.value}
+                key={value}
+                value={value}
                 className={"dropdown-item "}
-                onClick={event => {
-                  item.handleClick(event);
-                  setIsOpen(false);
-                }}
+                onClick={event => click(event, handleClick)}
               >
-                {item.label}
+                {label}
               </button>
             </div>
           ))}
