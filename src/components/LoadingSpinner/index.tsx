dc4d827324cd13e979743072ab318f84dac0aac3
Efe Kahyaoğlu <75262924+Efekahya@users.noreply.github.com>
2022-07-28 17:17:56 +0300

    fix style

--- src/components/LoadingSpinner/index.tsx
+++ src/components/LoadingSpinner/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import spinner from "../../assets/spinner.png";
 import "./styles.scss";
+
 export default function LoadingSpinner() {
   return (
-    <div className="container">
+    <div className="spinner--container">
       <img src={spinner} alt="loading.." />
     </div>
   );
