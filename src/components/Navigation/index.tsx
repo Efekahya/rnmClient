6b713514f3bf34834f16648d1b099289f4ed579a
Efe Kahyaoğlu <75262924+Efekahya@users.noreply.github.com>
2022-07-29 16:17:57 +0300

    moved href calculations before render func

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -10,28 +10,25 @@ for (let i = 0; i < href.length; i++) {
 
 let temp: string
 
-export default function Navigation() {
-  return (
-    <div className="navigation--navigationContainer">
-      {href.map((item, index) => {
-        for (let i = 0; i < href.length; i++) {
-          if (href[i] === item) {
-            if (i === 0) {
-              temp = href[0]
-            } else {
-              temp = href.slice(0, i + 1).join("/")
-            }
-          }
-        }
-        return (
-          <>
-            <div key={index} className="navigation--navigationText">
-              <a href={"/" + temp}>{hrefUpper[index]}</a>
-            </div>
-            <RightArrow />
-          </>
-        )
-      })}
+const urlsArray = href.map((url, index) => {
+  if (index === 0) {
+    temp = url
+    return url
+  } else {
+    temp += "/" + url
+    return temp
+  }
+})
+
+const elements = urlsArray.map((url, index) => (
+  <>
+    <div key={index} className="navigation--navigationText">
+      <a href={"/" + url}>{hrefUpper[index]}</a>
     </div>
-  )
+    <RightArrow />
+  </>
+))
+
+export default function Navigation() {
+  return <div className="navigation--navigationContainer">{elements}</div>
 }
