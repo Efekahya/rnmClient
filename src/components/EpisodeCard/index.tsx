d07269e2416b4193495974ab314c308d3482e10b
Efe Kahyaoğlu <75262924+Efekahya@users.noreply.github.com>
2022-07-29 17:53:03 +0300

    Merge branch 'EpisodeCard'

--- src/components/EpisodeCard/index.tsx
+++ src/components/EpisodeCard/index.tsx
@@ -1,6 +1,42 @@
 import React from "react"
-import "./styles.scss"
+import { ReactComponent as StarIcon } from "../../assets/star.svg"
+import { ReactComponent as StarFilledIcon } from "../../assets/starFilled.svg"
+import { IEpisodeCardProps } from "../../types/interfaces"
 
-export default function EpisodeCard() {
-  return <div>EpisodeCard</div>
+export default function EpisodeCard({
+  episode,
+  date,
+  title,
+  description,
+  favorited,
+  handleSetFavorited,
+}: IEpisodeCardProps) {
+  return (
+    <div className="episodeCard--episodeContainer">
+      <div className="episodeCard--psuedo">
+        <button
+          className="episodeCard--favoriteButton"
+          onClick={handleSetFavorited} //TODO Send request to database to favorite/unfavorite
+        >
+          {favorited ? (
+            <StarFilledIcon className="episodeCard--inline" />
+          ) : (
+            <StarIcon className="episodeCard--inline" />
+          )}
+        </button>
+      </div>
+      <button className="episodeCard--card">
+        <div className="episodeCard--header">
+          <div className="episodeCard--info">
+            <div className="episodeCard--episode title">{episode}</div>
+            <div className="episodeCard--date">{date}</div>
+          </div>
+        </div>
+        <div className="episodeCard--body">
+          <div className="episodeCard--title">{title}</div>
+          <div className="episodeCard--secondaryText">{description}</div>
+        </div>
+      </button>
+    </div>
+  )
 }
--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -1,18 +1,26 @@
 export interface ICharacter {
-  name: string
-  id: number
-  gender: string
-  spacies: string
-  origin: string
-  type: string
-  image: string
-  location: string
-  episodes: object[]
+  name: string;
+  id: number;
+  gender: string;
+  spacies: string;
+  origin: string;
+  type: string;
+  image: string;
+  location: string;
+  episodes: object[];
 }
 
 export interface ICharacterListProps {
-  characters: ICharacter[] //TODO Characterler objesi gelince character tipi ile değiştirilecek
-  count: number
+  characters: ICharacter[]; //TODO Characterler objesi gelince character tipi ile değiştirilecek
+  count: number;
+}
+export interface IEpisodeCardProps {
+  episode: string;
+  date: string;
+  title: string;
+  description: string;
+  favorited: boolean;
+  handleSetFavorited: () => void;
 }
 
 export interface IAddFavoritesProps {
