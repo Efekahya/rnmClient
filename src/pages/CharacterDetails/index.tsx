c8cd4dae8f6193179195cfcab4d0ee21770a980f
Efe Kahyaoğlu <75262924+Efekahya@users.noreply.github.com>
2022-07-31 22:01:16 +0300

    Character details page completed
    Waiting for navbar component to be approved

--- src/pages/CharacterDetails/index.tsx
+++ src/pages/CharacterDetails/index.tsx
@@ -48,70 +48,72 @@ export default function CharacterDetails() {
   return (
     <>
       <div className="character-container">
-        <div className="character-details">
-          <div className="character-name-container">
-            <div className="character-name">
-              <div className="character-name-text">{data.character.name}</div>
-              <AddFavorites
-                favorited={false}
-                themeClass={"black transparent"}
-                toggleFavorite={() => {
-                  console.log("here");
-                }}
-              />
+        <div className="character-details-container">
+          <div className="character-details">
+            <div className="character-name-container">
+              <div className="character-name">
+                <div className="character-name-text">{data.character.name}</div>
+                <AddFavorites
+                  favorited={false}
+                  themeClass={"black transparent"}
+                  toggleFavorite={() => {
+                    console.log("here");
+                  }}
+                />
+              </div>
             </div>
-          </div>
-          <div className="character-info-container">
-            <div className="character-image">
-              <img
-                src={data.character.image}
-                alt={data.character.name}
-                className="character-image"
-              />
-            </div>
-            <div className="character-info">
-              <CharacterDetailCard
-                title="Status"
-                content={data.character.status}
-              />
-              <CharacterDetailCard
-                title="Gender"
-                content={data.character.gender}
-              />
-              <CharacterDetailCard
-                title="Species"
-                content={data.character.species}
-              />
-              <CharacterDetailCard
-                title="Origin"
-                content={data.character.origin.name}
-              />
-              <CharacterDetailCard
-                title="Type"
-                content={
-                  data.character.type === "" ? "Unkown" : data.character.type
-                }
-              />
-              <CharacterDetailCard
-                title="Location"
-                content={data.character.location.name}
-              />
+            <div className="character-info-container">
+              <div className="character-image">
+                <img
+                  src={data.character.image}
+                  alt={data.character.name}
+                  className="character-image"
+                />
+              </div>
+              <div className="character-info">
+                <CharacterDetailCard
+                  title="Status"
+                  content={data.character.status}
+                />
+                <CharacterDetailCard
+                  title="Gender"
+                  content={data.character.gender}
+                />
+                <CharacterDetailCard
+                  title="Species"
+                  content={data.character.species}
+                />
+                <CharacterDetailCard
+                  title="Origin"
+                  content={data.character.origin.name}
+                />
+                <CharacterDetailCard
+                  title="Type"
+                  content={
+                    data.character.type === "" ? "Unkown" : data.character.type
+                  }
+                />
+                <CharacterDetailCard
+                  title="Location"
+                  content={data.character.location.name}
+                />
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <div className="character-episode-container">
-        <div className="character-episode-counter">
-          <ShowCount title="Episodes" count={data.character.episode.length} />
-        </div>
-        <div className="character-episode-items">
-          {episodes}
-          <div className="character-showmore-psuedo">
-            <div className="character-showmore-button">
+      <div className="character-episode-frame">
+        <div className="character-episode-container">
+          <div className="character-episode-showmore-psuedo">
+            <div className="character-episode-showmore">
               <a href="/episodes">
                 <Arrow />
               </a>
             </div>
+          </div>
+          <div className="character-episode">
+            <ShowCount title="Episodes" count={data.character.episode.length} />
+            <div className="character-episode-items">{episodes}</div>
           </div>
         </div>
       </div>
