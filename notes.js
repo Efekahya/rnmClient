afc2aa7801f747e9782f2527a5711ce54a12053a
Efe Kahyaoğlu <75262924+Efekahya@users.noreply.github.com>
2022-08-02 13:49:19 +0300

    changed the behavior of how the data is fetched initially
    Component is now fetching more data if the window height is smaller than the content height to ensure that the document is scrollable

---
+++ notes.js
@@ -0,0 +1,16 @@
+//*----------------------ShowCount
+//  TODO: Need to onclick action to showCount component to redirect to the page that it refers to
+
+
+//*----------------------EpisodeCard
+//  TODO: Make episode card component to redirect to the paga that it refers to with episode id 
+
+
+//*----------------------CharacterList
+//? FIXME: On hover component shows the origin and species info but width usually is not enough and texts collides with eacother
+
+
+
+
+
+//*----------------------
--- src/pages/Characters/index.tsx
+++ src/pages/Characters/index.tsx
@@ -1,15 +1,36 @@
-import React, { useEffect } from "react";
+import React, { MutableRefObject, useEffect, useRef } from "react";
 import "./styles.scss";
 import { useQuery } from "@apollo/client";
 import { GetCharacters } from "../../queries/queries";
 import ShowCount from "../../components/ShowCount";
 import CharacterList from "../../components/CharacterList";
 import { ICharacter, IInfo } from "../../types/interfaces";
+import LoadingSpinner from "../../components/LoadingSpinner";
 
 export default function Characters() {
   const [characters, setCharacters] = React.useState<ICharacter[]>([]);
   const [info, setInfo] = React.useState<IInfo>();
   const { data, loading, error, refetch } = useQuery(GetCharacters, {});
+
+  useEffect(() => {
+    setTimeout(() => {
+      const fetchMore = (i: number) => {
+        console.log("loading...");
+        refetch({
+          page: i++
+        });
+        if (window.innerHeight > document.body.offsetHeight) {
+          setTimeout(() => {
+            fetchMore(i);
+            i++;
+          }, 1000);
+        }
+      };
+      if (window.innerHeight > document.body.offsetHeight) {
+        fetchMore(2);
+      }
+    }, 1500);
+  }, [refetch]);
 
   useEffect(() => {
     if (loading === false && data) {
@@ -18,16 +39,18 @@ export default function Characters() {
     }
   }, [loading, data]);
 
-  if (loading) return <p>Loading...</p>;
+  if (loading) return <LoadingSpinner />;
   if (error) return <p>Error :(</p>;
 
   if (loading === false && data && info) {
     window.onscroll = e => {
+      console.log("scrolling");
       e.preventDefault();
       if (
-        window.innerHeight + document.documentElement.scrollTop ===
+        window.innerHeight + document.documentElement.scrollTop + 40 >
         document.documentElement.offsetHeight
       ) {
+        console.log("bottom");
         if (info.next !== null) {
           refetch({
             page: info.next
