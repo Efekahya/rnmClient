492ea5e1d6c44251fb61c6c2c38d4b7879079bad
Efe Kahyaoğlu <75262924+Efekahya@users.noreply.github.com>
2022-08-04 11:25:41 +0300

    Update queries.ts

--- src/queries/queries.ts
+++ src/queries/queries.ts
@@ -9,11 +9,38 @@ export const GetEpisodes = gql`
         next
       }
       results {
+        name
+      }
+    }
+  }
+`;
+
+export const GetCharacter = gql`
+  query GetCharacters($id: ID!) {
+    character(id: $id) {
+      id
+      name
+      status
+      species
+      type
+      gender
+      origin {
+        name
+      }
+      location {
+        name
+      }
+      image
+      episode {
         id
         name
         air_date
         episode
-}
+      }
+      created
+    }
+  }
+`;
 
 export const NavbarSearch = gql`
   query Characters($filter: FilterCharacter!) {
